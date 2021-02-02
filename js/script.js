74be7ed508ff770b01f59bed33053ad6dc01dcf1
German Eksi <german.eksi@gmail.com>
2021-02-02 15:38:45 +0200

    Minor changes to the preloading logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 gsap.to(".logo", { duration: 2, x: 300, y: 150 });
+
+
+//Display content when the page is loaded
+
+// show page function 
+function showPage() {
+    document.body.removeAttribute("state-before-laod");
+    
+}
