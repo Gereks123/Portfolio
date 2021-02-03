e7fe89570513c72dd17430261799606360c0179e
German Eksi <german.eksi@gmail.com>
2021-02-03 02:08:09 +0200

    Added preloader, added images, modified gsap

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
-gsap.to(".logo", { duration: 2, x: 300, y: 150 });
+const tl = gsap.timeline({ yoyo: true, repeat: -1 });
+//gsap stagger delays the function
+//Letter "G" animation
+tl.from(".letter1", {
+  duration: 0.2,
+  y: 10,
+  stagger: 0.25,
+  opacity: 100,
+});
 
+//Letter "E"´animation
+tl.from(".letter2", {
+  duration: 0.2,
+  y: 10,
+  stagger: 0.25,
+  opacity: 100,
+});
 
+//Check out gsap DOC https://greensock.com/docs/v3/GSAP
+//Check out gsap common mistakes https://greensock.com/mistakes/
 //Display content when the page is loaded
 
-// show page function 
+// show page function
 function showPage() {
-    document.body.removeAttribute("state-before-laod");
-    
+  document.body.classList.remove("state-before-load");
 }
+
+//Remove the styling when the page is loaded
+window.onload = (event) => {
+  showPage();
+};
